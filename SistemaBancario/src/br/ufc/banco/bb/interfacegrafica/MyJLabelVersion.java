ba7a8c71ce2ce2974792c0077eb3f9eac0496fa2
JonasMartins <jonas.s.martins@hotmail.com>
2015-12-09 20:17:55 -0300

    MyLabelVersion

---
+++ SistemaBancario/src/br/ufc/banco/bb/interfacegrafica/MyJLabelVersion.java
@@ -0,0 +1,17 @@
+package br.ufc.banco.bb.interfacegrafica;
+
+import java.awt.Font;
+
+import javax.swing.JLabel;
+
+@SuppressWarnings("serial")
+public class MyJLabelVersion extends JLabel{
+
+	
+	public MyJLabelVersion(String text, Font fonte){
+		setText(text);
+		setFont(fonte);
+	}
+	
+	
+}
